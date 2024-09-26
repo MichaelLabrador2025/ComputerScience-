afc4864f3790988d2d08c9ac84452f926addd4ac
MichaelLabrador2025 <818865@mystma.org>
2024-09-26 15:17:00 -0500

    sept 26 MadLib

---
+++ Assignments/Animalsurvey.py
@@ -0,0 +1,8 @@
+fav_animal = input("whats your favorite animal:\n>") #  person inputs fav animal
+Why = input("why do you like them:\n>") #   user inputs why they like the animal
+live = input("where do they live:\n>") #    user inputs where the animal lives
+fun = input("whats a fun fact about your animal\n>") #  a fun fact about the animal
+color = input("what color is your favorite animal:\n>") #   the color they come in 
+print ("your favorite animal is " + fav_animal + " you like "  + fav_animal  +  " because "  + Why + " they live " +  live + " a fun fact about your animial is "+ fun + " they are the color "  + color)
+#   a small sentence about what they input
+
---
+++ Unit 2 - Functions and Variables/Practice_sept-24.py
@@ -0,0 +1,6 @@
+my_variable = 4    #Declaration - create
+print(my_variable)
+my_variable = 5 #Assighment - modify
+print(my_variable)
+
+
