fd6f29a0914b973e4d25d4a0fc779919f09f0afd
MichaelLabrador2025 <818865@mystma.org>
2024-10-03 15:16:55 -0500

    test

---
+++ test/Unit_2.3_test.py
@@ -0,0 +1,7 @@
+def data_three():
+    word = input("enter a word\n>")
+    inten = input(int("enter a whole number\n>"))
+    rootbeer = input(float("enter a num with dec\n>"))
+    return (inten + rootbeer, word)
+
+print(data_three)
