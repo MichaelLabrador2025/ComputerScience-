dda359c022b8316e3b6be489dd099078be044117
MichaelLabrador2025 <818865@mystma.org>
2024-10-04 14:59:22 -0500

    bool's

---
+++ Unite 3/Bool_quiz.py
@@ -0,0 +1,22 @@
+answer1 = int(input("give a whole number that is leses than 5.\n>"))
+print(answer1<5)
+
+answer2 = input("who is the best teacher?\n>")
+if (answer2 == "none"):
+    print("true")
+else:
+    print("incorrect")
+
+answer3 = input("what does a concatonated ""cat + dog"" equal on python\n>")
+if (answer3 == "catdog"):
+    print("true")
+else:
+    print("flase")
+
+answer4 = int(input("give a number greater than or equal to 6\n>"))
+print(answer4>=6)
+
+answer5 = float(input("not equal to 3.14159\n>"))
+print(answer5 != 3.14159)
+
+
