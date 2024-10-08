fcd877711318e481e57f5d5fdcbcc4fda6a1a9b4
MichaelLabrador2025 <818865@mystma.org>
2024-10-08 15:15:06 -0500

    BOOL_test

--- Unit 3/Bool_quiz.py
+++ Unit 3/Bool_quiz.py
@@ -1,22 +1,46 @@
+"""string functions """
+#are a group of funtions that modify strings
+#  .lower()
+# .lower() changes the string to be all lower
+
+# .upper() changes the string to uppercase
+# . upper()
+
+# .capitalize() capilizes the first letter
+#("HELLO WORLD") --> "Hello world"
+
+# . title() changes the entire string to title case (Caps first letter of world)
+# "hello world" --> "Hello World"
+
+# . Swapcase() inverts the capitalization of each character
+# "Hello World" --> "hELLO wORLD"
+
+x = "Lord of the Rings".lower()
+x = x.lower()
+print(x)
+
+
 answer1 = int(input("give a whole number that is less than 5.\n>"))
-print(answer1<5)
+answer2 = int(input("give a whole number that is equal to 5.\n>"))
+answer3 = int(input("give a whole number that is greater than 5.\n>"))
+answer4 = int(input("give a whole number that is not 5.\n>"))
+answer5 = int(input("give a whole number that is less than or equal too 5.\n>"))
 
-answer2 = int(input("give a number equal to 1?\n>"))
-if (answer2 == 1):
-    print("true")
-else:
-    print("incorrect")
+def tally_score():
+    score = 0
+    if answer1 < 5:
+        score = score + 1
+    if answer2 == 5:
+        score = score + 1
+    if answer3 > 5:
+        score = score + 1
+    if answer4 != 5:
+        score = score + 1
+    if answer5 <= 5:
+        score = score + 1
+    print("score = "+ str(score))
+tally_score()
 
-answer3 = input("what does a concatonated ""cat + dog"" equal on python\n>")
-if (answer3 == "catdog"):
-    print("true")
-else:
-    print("flase")
 
-answer4 = int(input("give a number greater than or equal to 6\n>"))
-print(answer4>=6)
-
-answer5 = float(input("not equal to 3.14159\n>"))
-print(answer5 != 3.14159)
 
 
