a0be40668264131890131c8e907a9401765db94b
MichaelLabrador2025 <818865@mystma.org>
2024-10-08 14:33:32 -0500

    Unite_2_test

---
+++ test/test/Unit_2.1_test.py
@@ -0,0 +1,4 @@
+ranword1 = input ("please give me one random word:\n>")
+ranword2 = input ("please give me another random word:\n>")
+ranword3 = input ("please give me one final random word:\n>")
+print (ranword1 + ranword2 + ranword3 )
---
+++ test/test/Unit_2.2_test.py
@@ -0,0 +1,10 @@
+x = int(input("input num:"))
+y = int(input("input num:"))
+z = int(input("input num:"))
+add_three = (x + y + z)
+print (add_three)
+
+
+"""x = int(input("input number 1:\n>"))
+y = int(input("input number 2:\n>"))
+y = int(input("input number 3:\n>")) """
