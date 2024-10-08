efa6d4f612c7b86d8430c4870210183cba6abcd2
MichaelLabrador2025 <818865@mystma.org>
2024-10-08 14:20:36 -0500

    Bool_quiz

---
+++ Unit 3/booleanexpressions.py
@@ -0,0 +1,9 @@
+# Boolean Expressions
+# Mathematical formula kinda 
+# 14 + 10 = 24               Algebra Uses arithmatic operators
+# can only evaluate to True or False
+# 5 > 10 = False             Boolean algerbra used comparison operators
+# > < >= <= == !=
+
+x = 5                       # SET X equal to 5, tell it what to be
+print(x == 5)               # Get x equal to 5, ask a question
