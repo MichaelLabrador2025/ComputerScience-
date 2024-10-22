9df6132cc37e859adafdbc91b02873a22deac900
MichaelLabrador2025 <818865@mystma.org>
2024-10-22 15:19:50 -0500

    lil.john

--- Unit 3/lil_john.py
+++ Unit 3/lil_john.py
@@ -12,12 +12,32 @@ def start():
     return()
 
 def dayone():
-    print("its 6am acording to your sons basic survival sun clock. Your watch is all wonky and the radio keeps playing fien. the sun is rising fast and you and your son are sitting on the beach")
+    print("its 6am acording to your sons basic survival sun clock. Your watch is all wonky and the radio keeps playing fien." +
+           "the sun is rising fast and you and your son is sitting outside the plane" +
+          "you hear a russle in the woods")
     print("whats your next step")
     print("1. go to the beach and try to look for planes or other things that have dropped off the plane")
     print("2. go to the forest to collect sticks and other things to start a fire")
     print("3. go in the plane to try and grab supplsies")
     adventure = int(input(">"))   
     if adventure == 1:
-        
+        beach_begining()
+    elif adventure == 2:
+        forest_begining()
+
+def beach_begining():
+    print("you desided to go to the beach with lil john while he is playing with sea shells you look up at the sky"+ 
+      " and realize that the whole island is surounded by thick clouds and thunderstorms. but the sun perfeclty hits the island. It has been an hour and in the distanace you see a cave with a large entrance" + 
+      "it appears to be glowing lil john says he is hungry, but you really want to chech it out. in the distance you hear a strange noise in the forest.")
+    input("1. do you go to the forest" + 
+          "2. do you go back to the plane" + 
+          "3. do you check out the cave")
+def forest_begining ():
+    print("you desided to check out the noise"+
+          "its a dog!! you ponder why its sitting out here when there is no sign of civilization"+
+          "he starts barking and you look up............. zombies start chasing you!!!!" + " you tell lil john to get in the plane which is abt 100 meters and the dog comes with you")
+    input(1. )
+
+
+
 start()
